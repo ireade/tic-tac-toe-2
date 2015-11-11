8cd77959c3d33b174980279eb78fd0716fa2a2f7
Ire Aderinokun <ire@ireaderinokun.com>
2015-11-11 08:54:50 +0100

    clean up

--- tictactoe.js
+++ tictactoe.js
@@ -62,7 +62,6 @@ $(document).ready(function() {
 	}
 
 
-	
 
 	/* Manage the Win Table */
 
@@ -89,7 +88,7 @@ $(document).ready(function() {
 
 
 
-	/* Register is winning combination has happened */
+	/* Register if winning combination has happened */
 
 	function registerWin(x, y, z) {
 
@@ -159,10 +158,57 @@ $(document).ready(function() {
 		O PLAY
 	******************* */
 
-	function Orandomplay() {
+	/* Funciton to check if 2 of 3 winning squares have been played by the same player */
+
+	function checkTacticalPlay(x, y, z) {
+
+		// x and y played
+		if ( 
+			// 2 out of the 3 winning squares
+			$('.square[data-square="'+x+'"]').data("played") === true &&
+			$('.square[data-square="'+y+'"]').data("played") === true
+
+			&&
+
+			// Both squares played by the same player
+			$('.square[data-square="'+x+'"]').data("player") === $('.square[data-square="'+y+'"]').data("player")
+			) {
+
+			// Return remaining square
+			return z;
+		} 
+
+		// x and z played
+		else if ( 
+			$('.square[data-square="'+x+'"]').data("played") === true &&
+			$('.square[data-square="'+z+'"]').data("played") === true
+			&&
+			$('.square[data-square="'+x+'"]').data("player") === $('.square[data-square="'+z+'"]').data("player")
+			) {
+			return y;
+		}
+
+		// z and y played
+		else if ( 
+			$('.square[data-square="'+z+'"]').data("played") === true &&
+			$('.square[data-square="'+y+'"]').data("played") === true
+			&&
+			$('.square[data-square="'+z+'"]').data("player") === $('.square[data-square="'+y+'"]').data("player")
+			) {
+
+			return x;
+		} 
+
+		else {
+			return false;
+		}
+	}
+
+	/* Random play for O if no tactical play available */
+	function ORandomPlay() {
 
 		// Loop to find a valid play
-		for (var i = 0; i < 200; i++) {
+		for (var i = 0; i < 100; i++) {
 		
 			var n = Math.floor((Math.random() * 9) + 1);
 			if ( checkIfSquareFree( n ) ) {
@@ -180,13 +226,49 @@ $(document).ready(function() {
 	}
 
 
+	function OPlay() {
+
+		// Loop through all winning combinations
+		for (i = 0; i < wins.length; i++) {
+
+			// Check for tactical play
+			var tacticalPlay = checkTacticalPlay(wins[i][0], wins[i][1], wins[i][2]);
+
+
+			if (tacticalPlay) {
+
+				if ( checkIfSquareFree( tacticalPlay ) ) {
+
+					$('.square[data-square="'+tacticalPlay+'"').addClass("Oplayed");
+					$('.square[data-square="'+tacticalPlay+'"').addClass("played");
+					$('.square[data-square="'+tacticalPlay+'"').data("played", true);
+					$('.square[data-square="'+tacticalPlay+'"').data("player", "O");
+
+					checkWin();
+					break;
+
+				} else {
+					ORandomPlay();
+				}
+			}
+
+
+			if ( !tacticalPlay && i === (wins.length - 1) ) {
+				ORandomPlay();
+			}
+		} // end loop
+
+	}
+
+
+
+	
+
 	/* *******************
 		X PLAY
 	******************* */
 
 	$(".square").on("click", function() {
-
-
 
 		if ( checkIfSquareFree( $(this).data("square") ) ) {
 
@@ -198,7 +280,7 @@ $(document).ready(function() {
 
 			checkWin();
 
-			Orandomplay();
+			OPlay();
 
 		} else {
 			alert("Square already played. Please try again.")
